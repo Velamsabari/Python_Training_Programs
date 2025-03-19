c65b54864d3048992e5c96184ebb4e51137787cc
VELAM V <44916283+Velamsabari@users.noreply.github.com>
2025-03-19 18:44:51 +0530

    Add files via upload

---
+++ UserInput_Tuple.py
@@ -0,0 +1,8 @@
+name = input("Enter your name :\n")
+age = input("Enter your age :\n")
+gender = input("Enter your gender :\n")
+city = input("Enter your city :\n")
+japLevel = input("Enter your Japanese level :\n")
+
+myTuple = (name,age,gender,city,japLevel)
+print (myTuple)
