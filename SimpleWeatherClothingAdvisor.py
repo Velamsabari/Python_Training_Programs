66c82776604d334753896ffe50e74c8612d8e670
VELAM V <44916283+Velamsabari@users.noreply.github.com>
2025-03-28 12:47:27 +0530

    Add files via upload

---
+++ SimpleWeatherClothingAdvisor.py
@@ -0,0 +1,9 @@
+temp = int(input("Enter the temperature in celciius : \n"))
+if(temp < 10):
+    print("It's cold! Wear a warm coat and scarf.")
+elif(temp <= 25):
+    print("It's mild! A light jacket will do.")
+else:
+    print("It's hot! T-shirt would be perfect.")
+
+
